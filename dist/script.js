d13ee3fdfcb058bc00443f5b98bf4d6db5cad5a1
Viacheslav Urdzik <pc-631@macbook-pro-pc-631.local>
2024-02-15 09:39:44 +0200

    Initial commit

--- dist/script.js
+++ dist/script.js
@@ -2,13 +2,13 @@
 var scriptElement = document.createElement("script");
 scriptElement.type = "module";
 scriptElement.crossOrigin = true;
-scriptElement.src = "https://sqpine.github.io/rainbow-button/dist/assets/index-j-tZc8kk.js";
+scriptElement.src = "https://urdzik.github.io/rainbow-button-master/dist/assets/index-j-tZc8kk.js";
 
 // Create a new link element for CSS
 var linkElement = document.createElement("link");
 linkElement.rel = "stylesheet";
 linkElement.crossOrigin = true;
-linkElement.href = "https://sqpine.github.io/rainbow-button/dist/assets/index-iGirBDd0.css";
+linkElement.href = "https://urdzik.github.io/rainbow-button-master/dist/assets/index-iGirBDd0.css";
 
 // Append the script and link elements to the head of the document
 document.head.appendChild(scriptElement);
