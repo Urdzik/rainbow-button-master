dcaca7d814a8ac7b1c9cc469745ce6e802d3ec08
Viacheslav Urdzik <viacheslav.urdzik@gmail.com>
2024-03-06 14:46:30 +0200

    new link

--- src/connect-wallet-button.tsx
+++ src/connect-wallet-button.tsx
@@ -3,13 +3,17 @@ import {useAccount} from "wagmi";
 import {useEffect} from "react";
 
 export const ConnectWalletButton = () => {
-    const {  address } = useAccount();
+    const { address, isConnected, disconnect } = useAccount();
 
     useEffect(()=>{
+        if (!window.onUserDisconnected) return
+        if (!isConnected) {
+            window.onUserDisconnected()
+        }
         if(!address||!window?.onWalletConnected) return
 
         window.onWalletConnected(address)
-    },[address])
+    },[address, isConnected, disconnect])
 
     return <ConnectButton/>
 }
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,5 +5,6 @@ export {}
 declare global {
   interface Window {
     onWalletConnected: (address: string) => void
+    onUserDisconnected: () => void
   }
 }
